97e9810c78775f72a617fe8e272a89a5792f2b55
Lucas Cherkewski <lucas@ecustom.ca>
2018-09-08 22:00:46 -0400

    remove CSV bit

--- analyze.R
+++ analyze.R
@@ -2,9 +2,6 @@ library(tidyverse)
 library(geojsonio)
 library(leaflet)
 
-buildings <- read_csv("city-facilities.csv")
-fire_stations <- buildings %>% filter(BUILDING_ELEMENT_TYPE == "Fire Station")
-
 buildings_geo <- geojson_read("http://data.ottawa.ca/dataset/94d96532-bda5-4cb4-908c-16113cc94380/resource/838dc5e3-a0c4-4897-b3fe-cc0574e55083/download/city-facilities.json", what="sp")
 fire_stations_geo <- subset(buildings_geo, BUILDING_4 == "Fire Station")
 
